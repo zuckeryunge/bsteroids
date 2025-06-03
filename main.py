44da24c19804e8a0bc6b889ed26771c1a1c9e7de
zuckeryunge <mail@niconiermann.de>
2025-06-03 16:43:00 +0200

    can open and close

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-from typing import Deque
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
