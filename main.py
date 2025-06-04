1ad81225e68ed8b3e52fa024e220f94cd4d69f95
zuckeryunge <mail@niconiermann.de>
2025-06-04 09:29:45 +0200

    movement works

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
--- player.py
+++ player.py
@@ -18,3 +18,24 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen,"white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation = self.rotation + (PLAYER_TURN_SPEED * dt)
+
+    def move(self, dt):
+        go = pygame.Vector2(0 ,1).rotate(self.rotation)
+        go = go * (PLAYER_SPEED * dt)
+        print("lol works")
+        self.position = self.position + go
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.move(dt) 
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt) 
+        if keys[pygame.K_d]:
+            self.rotate(dt)
